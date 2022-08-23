47e722e7c00e3405f15e91cde49faa89ea6ea3b0
Azim3triko <azim3triko@protonmail.com>
2022-08-23 19:14:21 +0000

    proyecto terminado

---
+++ src/js/component/Title.js
@@ -0,0 +1,11 @@
+
+
+import React from "react";
+
+export const Title = () => {
+	return (
+		<div style={{ marginLeft: "40%" }}>
+			<p style={{ color: "blue", fontSize: "80px" }}>{"Todos"}</p>
+		</div>
+	);
+};
